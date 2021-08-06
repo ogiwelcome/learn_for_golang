676e7e47679eef1751c027b3ed9f943545cbf803
patron <ogi.fordevelopment@gmail.com>
2021-08-06 10:47:28 +0900

    construct

--- hello.go
+++ hello.go
@@ -4,9 +4,21 @@ func add1(a *int) int {
 	*a = *a+1
 	return *a
 }
+type person struct {
+	name string
+	age int
+}
+func older(p1, p2 person)(person, int) {
+	if p1.age>p2.age {
+		return p1,p1.age-p2.age
+	}
+	return p2,p2.age-p1.age
+}
 func main() {
-	x := 3
-	fmt.Println("x = ",x)
-	x1 := add1(&x)
-	fmt.Println("x+1=",x1)
+	var tom person
+	tom.name, tom.age = "Tom", 18
+	bob := person{age:25,name:"Bob"}
+	//paul := person{"Paul",43}
+	tb_older,tb_diff := older(tom,bob)
+	fmt.Println(tb_older,tb_diff)
 }
